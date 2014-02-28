4d7ac7f68f8c3ae2395458a3aeeb6828b872e3ab
Lukas <l.neicelis@gmail.com>
2014-02-28 21:11:11 +0200

    added service provider

--- src/Luknei/Translations/TranslationsServiceProvider.php
+++ src/Luknei/Translations/TranslationsServiceProvider.php
@@ -1,34 +1,64 @@
 <?php namespace Luknei\Translations;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Translation\Translator;
 
 class TranslationsServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerLoader();
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+        $this->app->bindShared('translator', function($app)
+        {
+            $loader = $app['translation.loader'];
+
+            // When registering the translator component, we'll need to set the default
+            // locale as well as the fallback locale. So, we'll grab the application
+            // configuration so we can easily get both of these values from there.
+            $locale = $app['config']['app.locale'];
+
+            $trans = new Translator($loader, $locale);
+
+            $trans->setFallback($app['config']['app.fallback_locale']);
+
+            return $trans;
+        });
+    }
+
+    /**
+     * Register the translation line loader.
+     *
+     * @return void
+     */
+    protected function registerLoader()
+    {
+        $this->app->bindShared('translation.loader', function($app)
+        {
+            return new EloquentLoader();
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('translator', 'translation.loader');
+    }
 
 }
